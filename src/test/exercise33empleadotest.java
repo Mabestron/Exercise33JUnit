864f2193b154d90325d4c6573e26e1770767f951
Mabestron <ivangamaliel555@hotmail.com>
2019-11-08 11:50:40 -0600

    JUnit v2

--- src/test/exercise33empleadotest.java
+++ src/test/exercise33empleadotest.java
@@ -9,12 +9,12 @@ import com.exercise33empleado.model.Employee;
 class exercise33empleadotest {
 
 	@Test
-	
 	void testCalculateBrute1() 
 	{
 		fail("Not yet implemented");
 	}
 	
+	@Test
 	void testCalculateBrute2() 
 	{
 		Employee myEmployee = new Employee();
@@ -23,11 +23,12 @@ class exercise33empleadotest {
 		assertEquals(actualValue,expectedValue);
 	}
 	
+	@Test
 	void testCalculateBrute3() 
 	{
 		Employee myEmployee = new Employee();
-		double actualValue=myEmployee.calculateBrute("salesEmployee", 1000, 0);
-		double expectedValue=1200;
+		double actualValue=myEmployee.calculateBrute("salesManager", 1000, 0);
+		double expectedValue=1600;
 		assertEquals(actualValue,expectedValue);
 	}
 	
